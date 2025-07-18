6d737d02e999b4bc9de88555a4ea038197dcfb8d
chihyu18 <weiyuchi123@gmail.com>
2025-07-18 16:40:20 +0800

    done: mp3_1

--- kernel/proc.h
+++ kernel/proc.h
@@ -117,4 +117,5 @@ struct proc
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   /* mp3 TODO */
+  struct usyscall *usyscall; // User syscall data (PID)
 };
