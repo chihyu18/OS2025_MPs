6fec8e9d4794fd805d761204afe114af76aa82f2
chihyu18 <weiyuchi123@gmail.com>
2025-08-26 08:52:01 +0800

    fix vm.c/vmprint_walk: print *pte, not pte

--- kernel/vm.c
+++ kernel/vm.c
@@ -488,7 +488,7 @@ void vmprint_walk(pagetable_t pagetable, int level, uint64 va_base)
       for (int _ = 0; _ < (3 - level) * 2; _++)
         printf(" ");
       
-      printf("%d: pte=%p va=%p pa=%p", i, pte, va, pa);
+      printf("%d: pte=%p va=%p pa=%p", i, *pte, va, pa);
       if(*pte & PTE_S) {
         printf(" blockno=%p", PTE2BLOCKNO(*pte));
       }
